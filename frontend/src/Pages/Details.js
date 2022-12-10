b632ef911e809b909cd89eaf8be7293df07cad1c
Hamza <108789747+hamzajuwaihan@users.noreply.github.com>
2022-12-10 19:05:53 +0300

    get movie

--- frontend/src/Pages/Details.js
+++ frontend/src/Pages/Details.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Featured from '../components/Details/Featured'
 import DetailsStart from '../components/Details/DetailsStart'
 import OtherStart from '../components/Details/OtherStart'
+import { useParams } from 'react-router';
+import axios from 'axios';
 function Details() {
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    axios.get(`https://localhost:8000/api/movie/${movieId}`).then((res) => {
+      console.log(res.data);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }, []);
   return (
     <>
-    <Featured/>
-    <DetailsStart/>
-    <OtherStart/>
+      <Featured />
+      <DetailsStart />
+      <OtherStart />
     </>
   )
 }
