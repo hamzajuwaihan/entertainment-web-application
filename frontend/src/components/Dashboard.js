eeca579ef4b9bccb5d666207c31549eb2a819dc6
Hamza <108789747+hamzajuwaihan@users.noreply.github.com>
2022-12-10 17:01:30 +0300

    navBar fix

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,7 +4,7 @@ function Dashboard() {
     return (
         <>
             <div className="dashboard">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <nav class="navbar navbar-expand-lg bg-light">
                     <div class="container-fluid">
                         <a class="navbar-brand" href="#">Navbar</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,29 +19,32 @@ function Dashboard() {
                                     <a class="nav-link" href="#">Link</a>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         Dropdown
                                     </a>
-                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <ul class="dropdown-menu">
                                         <li><a class="dropdown-item" href="#">Action</a></li>
                                         <li><a class="dropdown-item" href="#">Another action</a></li>
-                                        <li><hr class="dropdown-divider" /></li>
+                                        <li><hr class="dropdown-divider"/></li>
                                         <li><a class="dropdown-item" href="#">Something else here</a></li>
                                     </ul>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                                    <a class="nav-link disabled">Disabled</a>
                                 </li>
                             </ul>
-                            <form class="d-flex">
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            <form class="d-flex" role="search">
+                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                    <button class="btn btn-outline-success" type="submit">Search</button>
                             </form>
                         </div>
                     </div>
                 </nav>
             </div>
             <h1>Dashboard</h1>
+            <div class="alert alert-primary" role="alert">
+                A simple primary alert—check it out!
+            </div>
         </>
 
     )
