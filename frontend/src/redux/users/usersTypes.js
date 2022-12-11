1293e98f8cc3880054e8a727ca625fe5478b16df
Hamza <108789747+hamzajuwaihan@users.noreply.github.com>
2022-12-11 21:22:10 +0300

    users edit and delete

--- frontend/src/redux/users/usersTypes.js
+++ frontend/src/redux/users/usersTypes.js
@@ -15,3 +15,17 @@ export const USER_LOGIN_FAILURE = 'USER_LOGIN_FAILURE';
 
 // logout action types
 export const USER_LOGOUT = 'USER_LOGOUT';
+
+//users action types
+export const GET_ALL_USERS_REQUEST = 'GET_ALL_USERS_REQUEST';
+export const GET_ALL_USERS_SUCCESS = 'GET_ALL_USERS_SUCCESS';
+export const GET_ALL_USERS_FAILURE = 'GET_ALL_USERS_FAILURE';
+
+export const EDIT_USER_REQUEST = 'EDIT_USER_REQUEST';
+export const EDIT_USER_SUCCESS = 'EDIT_USER_SUCCESS';
+export const EDIT_USER_FAILURE = 'EDIT_USER_FAILURE';
+
+//delete user action types
+export const DELETE_USER_REQUEST = 'DELETE_USER_REQUEST';
+export const DELETE_USER_SUCCESS = 'DELETE_USER_SUCCESS';
+export const DELETE_USER_FAILURE = 'DELETE_USER_FAILURE';
