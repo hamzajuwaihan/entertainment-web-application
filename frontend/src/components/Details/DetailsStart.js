e619043fd0692caf92b490f9b9d47d3dd182ce77
Majdalshanteer <108790509+Majdalshanteer@users.noreply.github.com>
2022-12-11 09:59:06 +0300

    add signle page and comments

--- frontend/src/components/Details/DetailsStart.js
+++ frontend/src/components/Details/DetailsStart.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function DetailsStart({title}) {
+function DetailsStart({title ,rating,overview,genre,runtime,popularity,release_date}) {
   return (
     <>
     <div className="game-details">
@@ -12,28 +12,33 @@ function DetailsStart({title}) {
       <div className="content">
         <div className="row">
           <div className="col-lg-6">
-            <div className="left-info">
-              <div className="left">
-                <h4>Fortnite</h4>
-                <span>Sandbox</span>
+            <div className="left-info h-100 " >
+              <div className="left mt-3">
+                <h4>{title}</h4>
+                <span>{genre}</span>
               </div>
-              <ul>
-                <li><i className="fa fa-star" /> 4.8</li>
-                <li><i className="fa fa-download" /> 2.3M</li>
+              <ul className='mt-3'>
+                <li><i className="fa fa-star" />Rating : {rating}</li><br/>
+                <li><i className="fa fa-download" /> Runtime : {runtime}min</li><br/>
+                <li><i className="fa fa-server" /> Popularity : {popularity}</li><br/>
+                <li><i className="fa fa-gamepad" /> Release Date : {release_date}</li>
+                
               </ul>
             </div>
           </div>
           <div className="col-lg-6">
-            <div className="right-info">
-              <ul>
+            <div className="right-info h-100">
+              <h3>overview</h3>
+              <p>{overview}</p>
+              {/* <ul>
                 <li><i className="fa fa-star" /> 4.8</li>
                 <li><i className="fa fa-download" /> 2.3M</li>
                 <li><i className="fa fa-server" /> 36GB</li>
                 <li><i className="fa fa-gamepad" /> Action</li>
-              </ul>
+              </ul> */}
             </div>
           </div>
-          <div className="col-lg-4">
+          {/* <div className="col-lg-4">
             <img src="/user/assets/images/details-01.jpg" alt style={{borderRadius: 23, marginBottom: 30}} />
           </div>
           <div className="col-lg-4">
@@ -41,20 +46,32 @@ function DetailsStart({title}) {
           </div>
           <div className="col-lg-4">
             <img src="/user/assets/images/details-03.jpg" alt style={{borderRadius: 23, marginBottom: 30}} />
-          </div>
-          <div className="col-lg-12">
+          </div> */}
+          {/* <div className="col-lg-12">
             <p>Cyborg Gaming is free HTML CSS website template provided by TemplateMo. This is Bootstrap v5.2.0 layout. You can make a <a href="https://paypal.me/templatemo" target="_blank">small contribution via PayPal</a> to info [at] templatemo.com and thank you for supporting. If you want to get the PSD source files, please contact us. Lorem ipsum dolor sit consectetur es dispic dipiscingei elit, sed doers eiusmod lisum hored tempor.</p>
-          </div>
-          <div className="col-lg-12">
+          </div> */}
+          {/* <div className="col-lg-12">
             <div className="main-border-button">
               <a href="#">Download Fortnite Now!</a>
             </div>
-          </div>
+          </div> */}
+
+
+
+
+
+
         </div>
       </div>
     </div>
   </div>
 </div>
+
+
+
+
+
+
 
     </>
   )
