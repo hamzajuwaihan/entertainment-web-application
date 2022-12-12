a46cfa832971ab3209cb5db1d8bff47f42531cea
Hamza <108789747+hamzajuwaihan@users.noreply.github.com>
2022-12-12 14:42:25 +0300

    featured

--- frontend/src/components/Details/Featured.js
+++ frontend/src/components/Details/Featured.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Featured({image,poster}) {
+function Featured({ image, poster }) {
   return (
     <>
 
@@ -9,14 +9,14 @@ function Featured({image,poster}) {
           <div className="feature-banner header-text">
             <div className="row ">
               <div className="col-lg-4 ">
-                <img src={`http://localhost:8000/images/${image}`} alt style={{ borderRadius: 23, height:"350px" }} />
+                <img src={`http://localhost:8000/images/${image}`} alt style={{ borderRadius: 23, height: "350px" }} />
               </div>
               <div className="col-lg-8"  >
                 <div className="">
-                  <img src={`http://localhost:8000/images/${poster}`} alt style={{ borderRadius: 23,height:"350px"  }} />
+                  <img src={`http://localhost:8000/images/${poster}`} alt style={{ borderRadius: 23, height: "350px" }} />
                   <a href="https://www.youtube.com/watch?v=r1b03uKWk_M" target="_blank">
                     {/* <i className="fa fa-play" /> */}
-                    </a>
+                  </a>
                 </div>
               </div>
             </div>
